33752660577446af5b3203b513e2c5027f3d0eb1
chenpengyu <chenpengyu_1024@163.com>
2023-12-12 16:43:07 +0800

    diagram get

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Three-step-reading
 ## first pass
-Geting title,
+Getting title,
 abstract,
 introduction,
 section and subsection,
 conclusion
+## second pass
+Get all paragraph, and figures, diagrams
+
+# Attention
++ Alter the `DISPLAY_MODE` to `True` to see the run result.
++ Specify the image output dir by setting `output_dir` 
 # Problem existing
 + In `CONCLUSION` part, Conclusion and Acknowledgments are in the same json object. 
++ Diagram with no corresponding name
--- parse.py
+++ parse.py
@@ -1,4 +1,7 @@
 import json
+import os
+from PIL import Image
+import openpyxl
 
 # Paper Content
 title = ''
@@ -7,6 +10,14 @@ introduction = ''
 section_list = []
 conclusion = ''
 paragraph_list = []
+image_list = []
+chart_list = []
+
+# specify the output dir
+output_dir = './output'
+
+# alter this to see the run result
+DISPLAY_MODE = False
 
 
 class Paragraphs:
@@ -16,18 +27,17 @@ class Paragraphs:
 
 
 def getParagraphs(json_obj):
-
     if isinstance(json_obj, dict):
         ttl = json_obj.get("Title", "")
         cont = json_obj.get("Content", "")
 
-        paragraph_list.append(Paragraphs(ttl,cont))
+        paragraph_list.append(Paragraphs(ttl, cont))
         below = json_obj.get("Below", [])
-        for item in below:
-            getParagraphs(item)
+        for content_item in below:
+            getParagraphs(content_item)
     elif isinstance(json_obj, list):
-        for item in json_obj:
-            getParagraphs(item)
+        for content_item in json_obj:
+            getParagraphs(content_item)
 
 
 if __name__ == '__main__':
@@ -37,14 +47,14 @@ if __name__ == '__main__':
     '''
     first pass
     '''
-    title = parsed_json['Title']
-    abstract = parsed_json['Abstract']
-    for section in parsed_json['Content']:
-        if section['Title'] == 'INTRODUCTION':
-            introduction = section['Content']
+    title = parsed_json.get('Title', '')
+    abstract = parsed_json.get('Abstract', '')
+    for section in parsed_json.get('Content', []):
+        if section.get('Title', '') == 'INTRODUCTION':
+            introduction = section.get('Content', '')
             break
     # presented by #title ##subtitle, but `conclusion` and `reference` is not included
-    for section in parsed_json['Content']:
+    for section in parsed_json.get('Content', []):
         section_list.append('#' + section['Title'])
         if len(section['Below']) != 0:
             for subSection in section['Below']:
@@ -62,9 +72,9 @@ if __name__ == '__main__':
     if idx != -1:
         conclusion = conclusion[:idx]
 
-    # for display
-    margin = '\n---------'
-    # print(title, margin, abstract, margin, section_list, margin, introduction, margin, conclusion)
+    if DISPLAY_MODE:
+        margin = '\n---------\n'
+        print(title, margin, abstract, margin, section_list, margin, introduction, margin, conclusion)
 
     '''
     second pass
@@ -72,7 +82,34 @@ if __name__ == '__main__':
 
     for section in parsed_json['Content']:
         if section['Title'] != 'INTRODUCTION':
-            # pg = Paragraphs(section['Title'], section['Content'])
             getParagraphs(section)
-    for item in paragraph_list:
-        print('name: ', item.name, '\n content:', item.content)
+
+    if DISPLAY_MODE:
+        print('## Paragraph content shows below ## \n')
+        for item in paragraph_list:
+            print('name: ', item.name, '\n content:', item.content)
+
+    page_num = 0
+
+    try:
+        sub_dir = os.listdir(output_dir)
+        page_num = len(sub_dir)
+        if page_num == 0:
+            print('No pages inside \n')
+        sub_dir = sorted(sub_dir, key=lambda x: int(x))
+
+        for root, dirs, files in os.walk(output_dir):
+            for file in files:
+                if file.lower().endswith(".jpg"):
+                    file_path = os.path.join(root, file)
+                    img = Image.open(file_path)
+                    image_list.append(img)
+                elif file.lower().endswith('.xlsx'):
+                    file_path = os.path.join(root, file)
+                    chart = openpyxl.load_workbook(file_path)
+                    chart_list.append(chart)
+    except FileNotFoundError:
+        print(f"Error: The folder '{output_dir}' does not exist.")
+
+    if DISPLAY_MODE:
+        print(f'The size of each list is {len(image_list)}, {len(chart_list)}')

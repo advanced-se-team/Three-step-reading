047ba53bafb7350c87d4da94489e15873d3d9127
chenpengyu <chenpengyu_1024@163.com>
2023-12-12 11:11:18 +0800

    second pass

--- parse.py
+++ parse.py
@@ -6,6 +6,29 @@ abstract = ''
 introduction = ''
 section_list = []
 conclusion = ''
+paragraph_list = []
+
+
+class Paragraphs:
+    def __init__(self, name, content):
+        self.name = name
+        self.content = content
+
+
+def getParagraphs(json_obj):
+
+    if isinstance(json_obj, dict):
+        ttl = json_obj.get("Title", "")
+        cont = json_obj.get("Content", "")
+
+        paragraph_list.append(Paragraphs(ttl,cont))
+        below = json_obj.get("Below", [])
+        for item in below:
+            getParagraphs(item)
+    elif isinstance(json_obj, list):
+        for item in json_obj:
+            getParagraphs(item)
+
 
 if __name__ == '__main__':
     with open('parsedFile.json') as json_file:
@@ -41,5 +64,15 @@ if __name__ == '__main__':
 
     # for display
     margin = '\n---------'
-    print(title, margin, abstract, margin, section_list, margin, introduction, margin, conclusion)
+    # print(title, margin, abstract, margin, section_list, margin, introduction, margin, conclusion)
 
+    '''
+    second pass
+    '''
+
+    for section in parsed_json['Content']:
+        if section['Title'] != 'INTRODUCTION':
+            # pg = Paragraphs(section['Title'], section['Content'])
+            getParagraphs(section)
+    for item in paragraph_list:
+        print('name: ', item.name, '\n content:', item.content)

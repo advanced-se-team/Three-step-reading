537a50863e91e2da46f45571f9d3648f8cfb1f62
chenpengyu <chenpengyu_1024@163.com>
2023-12-12 10:22:22 +0800

    first pass

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Three-step-reading
+## first pass
+Geting title,
+abstract,
+introduction,
+section and subsection,
+conclusion
+# Problem existing
++ In `CONCLUSION` part, Conclusion and Acknowledgments are in the same json object. 
--- parse.py
+++ parse.py
@@ -1,21 +1,45 @@
 import json
 
 # Paper Content
-Title = ''
-Abstract = ''
-Introduction = ''
+title = ''
+abstract = ''
+introduction = ''
+section_list = []
+conclusion = ''
 
 if __name__ == '__main__':
     with open('parsedFile.json') as json_file:
         parsed_json = json.load(json_file)
 
-    Title = parsed_json['Title']
-    Abstract = parsed_json['Abstract']
+    '''
+    first pass
+    '''
+    title = parsed_json['Title']
+    abstract = parsed_json['Abstract']
     for section in parsed_json['Content']:
-        if section['Title'] == 'Introduction':
-            Introduction = section['Content']
+        if section['Title'] == 'INTRODUCTION':
+            introduction = section['Content']
             break
+    # presented by #title ##subtitle, but `conclusion` and `reference` is not included
+    for section in parsed_json['Content']:
+        section_list.append('#' + section['Title'])
+        if len(section['Below']) != 0:
+            for subSection in section['Below']:
+                section_list.append('##' + subSection['Title'])
+    if '#CONCLUSION' in section_list:
+        section_list.remove('#CONCLUSION')
+    if '#REFERENCES' in section_list:
+        section_list.remove('#REFERENCES')
 
-    # print(parsed_json)
-    # print('#')
-    # print(Title, '\n', Abstract, '\n', Introduction)
+    for section in reversed(parsed_json['Content']):
+        if section['Title'] == 'CONCLUSION':
+            conclusion = section['Content']
+            break
+    idx = conclusion.find('Acknowledgments')
+    if idx != -1:
+        conclusion = conclusion[:idx]
+
+    # for display
+    margin = '\n---------'
+    print(title, margin, abstract, margin, section_list, margin, introduction, margin, conclusion)
+
